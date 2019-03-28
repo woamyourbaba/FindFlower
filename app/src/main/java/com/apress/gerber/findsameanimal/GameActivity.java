add35e04467b06cc2b0d89cfe3bcd1edf9d0fc56
谈力 <2436229858@qq.com>
2019-03-28 19:28:46 +0800

    others

--- app/src/main/java/com/apress/gerber/findsameanimal/GameActivity.java
+++ app/src/main/java/com/apress/gerber/findsameanimal/GameActivity.java
@@ -14,311 +14,181 @@ import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
+   int count=0;
+   int x=0;
+   private ImageView view0;
+    private ImageView view1;
+    private ImageView view2;
+    private ImageView view3;
+    private ImageView view4;
+    private ImageView view5;
+    private ImageView view6;
+    private ImageView view7;
+    private ImageView view8;
+    private ImageView view9;
+    private ImageView view10;
+    private ImageView view11;
+    private ImageView[] v=new ImageView[12];
+    private int[] ints=new int[]{
+            R.drawable.bear,R.drawable.bird,R.drawable.elephant,R.drawable.cat,
+            R.drawable.leo,R.drawable.tiger,
+            R.drawable.bear,R.drawable.bird,R.drawable.elephant,R.drawable.cat,
+            R.drawable.leo,R.drawable.tiger
+    };
+    private TextView textView;
+    private int[] random=testRandom();
+    private  ImageView[] ima=new ImageView[2];
+    private int[] flag=new int[2];
 
-   int x=1000;
+
    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        final  ImageView view1=(ImageView)findViewById(R.id.imageView);
-        final ImageView view2=(ImageView)findViewById(R.id.imageView2);
-        final ImageView view3=(ImageView)findViewById(R.id.imageView3);
-        final ImageView view4=(ImageView)findViewById(R.id.imageView4);
-        final ImageView view5=(ImageView)findViewById(R.id.imageView5);
-        final ImageView view6=(ImageView)findViewById(R.id.imageView6);
-        final ImageView view7=(ImageView)findViewById(R.id.imageView7);
-        final ImageView view8=(ImageView)findViewById(R.id.imageView8);
-        final ImageView view9=(ImageView)findViewById(R.id.imageView9);
-        final ImageView view10=(ImageView)findViewById(R.id.imageView10);
-        final ImageView view11=(ImageView)findViewById(R.id.imageView11);
-        final ImageView view12=(ImageView)findViewById(R.id.imageView12);
-        final TextView textView=(TextView)findViewById(R.id.textView2);
+        findview();
 
+    }
 
-        final int[] random=testRandom();
-        final int[] image=new int[]{
-                R.drawable.bear,R.drawable.bird,R.drawable.elephant,R.drawable.cat,
-                R.drawable.leo,R.drawable.tiger,
-                R.drawable.bear,R.drawable.flower,R.drawable.elephant,R.drawable.cat,
-                R.drawable.leo,R.drawable.tiger
-        };
+    private void findview(){
+         v[0]=(ImageView)findViewById(R.id.imageView);
+         v[1]=(ImageView)findViewById(R.id.imageView2);
+         v[2]=(ImageView)findViewById(R.id.imageView3);
+         v[3]=(ImageView)findViewById(R.id.imageView4);
+         v[4]=(ImageView)findViewById(R.id.imageView5);
+         v[5]=(ImageView)findViewById(R.id.imageView6);
+         v[6]=(ImageView)findViewById(R.id.imageView7);
+         v[7]=(ImageView)findViewById(R.id.imageView8);
+         v[8]=(ImageView)findViewById(R.id.imageView9);
+         v[9]=(ImageView)findViewById(R.id.imageView10);
+         v[10]=(ImageView)findViewById(R.id.imageView11);
+         v[11]=(ImageView)findViewById(R.id.imageView12);
+         textView=(TextView)findViewById(R.id.textView2);
+    }
 
-        view1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view1.setImageResource(image[random[0]]);
-                if(image[random[0]]!=R.drawable.flower){
-                    x=x-100;
-                    String str=Integer.toString(x);
+    private void setNum(){
+         int[] random=testRandom();
+         for (int i=0;i<12;i++)
+             v[i].setImageResource(ints[random[i]]);
+    }
+
+    public  void onClick(View view){
+       findview();
+        if(view.getId()==v[0].getId()){
+            v[0].setImageResource(ints[random[0]]);
+            ima[count]=v[0];
+            flag[count]=ints[random[0]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }else if(view.getId()==v[1].getId()){
+            v[1].setImageResource(ints[random[1]]);
+            ima[count]=v[1];
+            flag[count]=ints[random[1]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }else if(view.getId()==v[2].getId()){
+            v[2].setImageResource(ints[random[2]]);
+            ima[count]=v[2];
+            flag[count]=ints[random[2]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }else if(view.getId()==v[3].getId()){
+            v[3].setImageResource(ints[random[3]]);
+            ima[count]=v[3];
+            flag[count]=ints[random[3]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }
+        else if(view.getId()==v[4].getId()){
+            v[4].setImageResource(ints[random[4]]);
+            ima[count]=v[4];
+            flag[count]=ints[random[4]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }
+        else if(view.getId()==v[5].getId()){
+            v[5].setImageResource(ints[random[5]]);
+            ima[count]=v[5];
+            flag[count]=ints[random[5]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }else if(view.getId()==v[6].getId()){
+            v[6].setImageResource(ints[random[6]]);
+            ima[count]=v[6];
+            flag[count]=ints[random[6]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }else if(view.getId()==v[7].getId()){
+            v[7].setImageResource(ints[random[7]]);
+            ima[count]=v[7];
+            flag[count]=ints[random[7]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }else if(view.getId()==v[8].getId()){
+            v[8].setImageResource(ints[random[8]]);
+            ima[count]=v[8];
+            flag[count]=ints[random[8]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }else if(view.getId()==v[9].getId()){
+            v[9].setImageResource(ints[random[9]]);
+            ima[count]=v[9];
+            flag[count]=ints[random[9]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }else if(view.getId()==v[10].getId()){
+            v[10].setImageResource(ints[random[10]]);
+            ima[count]=v[10];
+            flag[count]=ints[random[10]];
+            count++;
+            if(count==2)
+                remo(ima[0],ima[1],flag[0],flag[1]);
+        }else if(view.getId()==v[11].getId()){
+            v[11].setImageResource(ints[random[11]]);
+            ima[count]=v[11];
+            flag[count]=ints[random[11]];
+            count++;
+            if(count==2)
+            remo(ima[0],ima[1],flag[0],flag[1]);
+        }
+
+        if(x==600){
+            Intent intent=new Intent(GameActivity.this,ResultActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void remo(ImageView v1, ImageView v2,int m1,int m2){
+        textView=(TextView)findViewById(R.id.textView2);
+
+           if (v1.getId() != v2.getId()) {
+               if (m1==m2) {
+                   v1.setVisibility(View.GONE);
+                   v2.setVisibility(View.GONE);
+                   count=0;
+                   x+=100;
+                   String str=Integer.toString(x);
                    textView.setText(str);
-                    if(x==0){
-                        Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
+               }
+               else {
+                   v1.setImageResource(R.drawable.house);
+                   v2.setImageResource(R.drawable.house);
+                   count=0;
+               }
+           }else
+               count=1;
 
-                }
-                else{
-                    Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-
-        view2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view2.setImageResource(image[random[1]]);
-                if(image[random[1]]!=R.drawable.flower){
-                    x=x-100;
-                    String str=Integer.toString(x);
-                    textView.setText(str);
-                    if(x==0){
-                        Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                }
-                else{
-                    Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-
-        view3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view3.setImageResource(image[random[2]]);
-                if(image[random[2]]!=R.drawable.flower){
-                    x=x-100;
-                    String str=Integer.toString(x);
-                    textView.setText(str);
-                    if(x==0){
-                        Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                }
-                else{
-                    Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-
-        view4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view4.setImageResource(image[random[3]]);
-                if(image[random[3]]!=R.drawable.flower){
-                    x=x-100;
-                    String str=Integer.toString(x);
-                    textView.setText(str);
-                    if(x==0){
-                        Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                }
-                else{
-                    Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-
-        view5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view5.setImageResource(image[random[4]]);
-                if(image[random[4]]!=R.drawable.flower){
-                    x=x-100;
-                    String str=Integer.toString(x);
-                    textView.setText(str);
-                    if(x==0){
-                        Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                }
-                else{
-                    Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-
-        view6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view6.setImageResource(image[random[5]]);
-                if(image[random[5]]!=R.drawable.flower){
-                    x=x-100;
-                    String str=Integer.toString(x);
-                    textView.setText(str);
-                    if(x==0){
-                        Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                }
-                else{
-                    Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-
-      view7.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              view7.setImageResource(image[random[6]]);
-              if(image[random[6]]!=R.drawable.flower){
-                  x=x-100;
-                  String str=Integer.toString(x);
-                  textView.setText(str);
-                  if(x==0){
-                      Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                      startActivity(intent);
-                      finish();
-                  }
-
-              }
-              else{
-                  Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                  startActivity(intent);
-                  finish();
-              }
-          }
-      });
-
-      view8.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              view8.setImageResource(image[random[7]]);
-              if(image[random[7]]!=R.drawable.flower){
-                  x=x-100;
-                  String str=Integer.toString(x);
-                  textView.setText(str);
-                  if(x==0){
-                      Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                      startActivity(intent);
-                      finish();
-                  }
-
-              }
-              else{
-                  Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                  startActivity(intent);
-                  finish();
-              }
-          }
-      });
-
-      view9.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              view9.setImageResource(image[random[8]]);
-              if(image[random[8]]!=R.drawable.flower){
-                  x=x-100;
-                  String str=Integer.toString(x);
-                  textView.setText(str);
-                  if(x==0){
-                      Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                      startActivity(intent);
-                      finish();
-                  }
-
-              }
-              else{
-                  Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                  startActivity(intent);
-                  finish();
-              }
-          }
-      });
-
-      view10.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              view10.setImageResource(image[random[9]]);
-              if(image[random[9]]!=R.drawable.flower){
-                  x=x-100;
-                  String str=Integer.toString(x);
-                  textView.setText(str);
-                  if(x==0){
-                      Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                      startActivity(intent);
-                      finish();
-                  }
-
-              }
-              else{
-                  Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                  startActivity(intent);
-                  finish();
-              }
-          }
-      });
-
-      view11.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              view11.setImageResource(image[random[10]]);
-              if(image[random[10]]!=R.drawable.flower){
-                  x=x-100;
-                  String str=Integer.toString(x);
-                  textView.setText(str);
-                  if(x==0){
-                      Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                      startActivity(intent);
-                      finish();
-                  }
-
-              }
-              else{
-                  Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                  startActivity(intent);
-                  finish();
-              }
-          }
-      });
-
-      view12.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              view12.setImageResource(image[random[11]]);
-              if(image[random[11]]!=R.drawable.flower){
-                  x=x-100;
-                  String str=Integer.toString(x);
-                  textView.setText(str);
-                  if(x==0){
-                      Intent intent=new Intent(GameActivity.this,FailActivity.class);
-                      startActivity(intent);
-                      finish();
-                  }
-
-              }
-              else{
-                  Intent intent=new Intent(GameActivity.this,ResultActivity.class);
-                  startActivity(intent);
-                  finish();
-              }
-          }
-      });
     }
 
     private int[] testRandom() {
